52070135685e103209944892c09ef08757bdfc5f
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-09-29 10:27:04 +0300

    Cryptocurrences Page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,42 +4,62 @@ import millify from "millify";
 // Components
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
+import { Cryptocurrencies, News } from "../pages";
 
 // Services
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
-  const globalStats = data?.data?.stats;
+    const { data, isFetching } = useGetCryptosQuery();
+    const globalStats = data?.data?.stats;
 
-  const Titles = [
-    "Total Cryptocurrencies",
-    "Total Coins",
-    "Total Markets",
-    "Total Exchanges",
-    "Total Market Cap",
-    "Total 24h Volume",
-  ];
-  return (
-    <>
-      <Title level={2} className="heading">
-        Global Crypto Stats
-      </Title>
-      {data && (
-        <Row>
-          {Object.keys(globalStats).map((state, i) => (
-            <Col span={12} key={state}>
-              <Statistic
-                title={Titles[i]}
-                value={millify(globalStats[state])}
-              />
-            </Col>
-          ))}
-        </Row>
-      )}
-    </>
-  );
+    const Titles = [
+        "Total Cryptocurrencies",
+        "Total Coins",
+        "Total Markets",
+        "Total Exchanges",
+        "Total Market Cap",
+        "Total 24h Volume",
+    ];
+    return (
+        <>
+            <Title level={2} className="heading">
+                Global Crypto Stats
+            </Title>
+            {data && (
+                <Row>
+                    {Object.keys(globalStats).map((state, i) => (
+                        <Col span={12} key={state}>
+                            <Statistic
+                                title={Titles[i]}
+                                value={millify(globalStats[state])}
+                            />
+                        </Col>
+                    ))}
+                </Row>
+            )}
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">
+                    Top 10 Cryptocurrencies in the world
+                </Title>
+                <Title level={3} className="show-more">
+                    <Link to="cryptocurrencies">Show More</Link>
+                </Title>
+            </div>
+            <Cryptocurrencies simplified />
+
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">
+                    Lates Crypto News
+                </Title>
+                <Title level={3} className="show-more">
+                    <Link to="cryptocurrencies">Show More</Link>
+                </Title>
+            </div>
+            <News simplified />
+        </>
+    );
 };
 
 export default Homepage;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-  "X-RapidAPI-Key": "ddb6f841c7msh0fe50938580ead8p15cae8jsnf1ee0409bc94",
-  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    "X-RapidAPI-Key": "ddb6f841c7msh0fe50938580ead8p15cae8jsnf1ee0409bc94",
+    "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
 };
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
@@ -11,11 +11,13 @@ const baseUrl = "https://coinranking1.p.rapidapi.com";
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
-  reducerPath: "cryptoApi",
-  baseQuery: fetchBaseQuery({ baseUrl }),
-  endpoints: (builder) => ({
-    getCryptos: builder.query({ query: () => createRequest("./coins") }),
-  }),
+    reducerPath: "cryptoApi",
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`./coins?limit=${count || 10}`),
+        }),
+    }),
 });
 
 export const { useGetCryptosQuery } = cryptoApi;
